abea5198a7d265757781b377d05cc9cdc3dab291
Clint Edwards <cedwards1080@gmail.com>
2021-08-26 15:03:09 -0600

    Added navigation and 1st section

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/main.js"));
+});
+
+
+
 const port = process.env.PORT || 4005
 
 
